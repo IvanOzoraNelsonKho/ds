016a6edf9381f9288d75e7ac8b51471601e92e5d
IvanOzoraNelsonKho <Vaannn1806@gmail.com>
2026-05-10 14:47:38 +0700

    2

--- src/GraphMain.java
+++ src/GraphMain.java
@@ -1,36 +1,66 @@
 import com.datastruct.*;
 
-class MyVertex{
+class MyVertex {
 	String nodeName;
-	MyVertex(String name)
-	{
+	int orderIndex;
+
+	MyVertex(String name, int orderIndex) {
 		this.nodeName = name;
+		this.orderIndex = orderIndex;
 	}
 
     @Override
     public String toString() {
         return (nodeName);
     }
-}
 
+    @Override
+    public int hashCode() {
+        return orderIndex;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyVertex other = (MyVertex) obj;
+        return this.nodeName.equals(other.nodeName);
+    }
+}
 
 public class GraphMain {
     public static void main(String[] args) {
-        //create vertex
-        MyVertex v1 = new MyVertex("a");
-        MyVertex v2 = new MyVertex("b");
-        MyVertex v3 = new MyVertex("c");
-        MyVertex v4 = new MyVertex("d");
-        MyVertex v5 = new MyVertex("e");
+        MyVertex vE = new MyVertex("E", 0);
+        MyVertex vD = new MyVertex("D", 1);
+        MyVertex vI = new MyVertex("I", 2);
+        MyVertex vC = new MyVertex("C", 3);
+        MyVertex vG = new MyVertex("G", 4);
+        MyVertex vF = new MyVertex("F", 5);
+        MyVertex vB = new MyVertex("B", 6);
+        MyVertex vH = new MyVertex("H", 7);
+        MyVertex vA = new MyVertex("A", 8);
 
-        Graph<MyVertex> WG = new Graph<MyVertex>(false); //undirected
-        WG.addEdge(v1, v2, 4);
-        WG.addEdge(v1, v3, 6);
-        WG.addEdge(v2, v3, 1);
-        WG.addEdge(v2, v4, 2);
-        WG.addEdge(v4, v5, 2);
-        WG.addEdge(v5, v3, 1);
+        Graph<MyVertex> WG = new Graph<MyVertex>(true);
 
-        System.out.println("Undirected Graph:"); WG.printGraph();
+        WG.addEdge(vE, vF, 1);
+        WG.addEdge(vE, vH, 1);
+        WG.addEdge(vD, vG, 1);
+        WG.addEdge(vI, vF, 1);
+        WG.addEdge(vC, vB, 1);
+        WG.addEdge(vG, vH, 1);
+        WG.addEdge(vF, vC, 1);
+        WG.addEdge(vF, vH, 1);
+        WG.addEdge(vB, vE, 1);
+        WG.addEdge(vH, vI, 1);
+        WG.addEdge(vA, vB, 1);
+        WG.addEdge(vA, vD, 1);
+        WG.addEdge(vA, vE, 1);
+
+        System.out.println("Directed Graph:"); 
+        WG.printGraph();
+        
+        System.out.println();
+        WG.DFS(vA);
+        WG.BFS(vA);
     }
 }
--- src/com/datastruct/Graph.java
+++ src/com/datastruct/Graph.java
@@ -1,11 +1,9 @@
 package com.datastruct;
-/* 
- * Struktur data Graph dengan bobot pada setiap edge
+/* * Struktur data Graph dengan bobot pada setiap edge
  * sources: https://www.lavivienpost.net/weighted-graph-as-adjacency-list/  
- * 
  */
 
-import java.util.*;
+import java.util.*; // Bawaan dari file asli praktikum, biarkan saja
 
 class Edge<T> { 
 	private T neighbor; //connected vertex
@@ -38,13 +36,12 @@ class Edge<T> {
 }
 
 public class Graph<T> { 
-    //Map<T, LinkedList<Edge<T>>> adj;
 	private Map<T, MyLinearList<Edge<T>>> adj;
 	boolean directed;
 	
 	//Constructor, Time O(1) Space O(1)
 	public Graph (boolean type) { 
-        adj = new HashMap<>();
+        adj = new HashMap<>(); // Tetap gunakan HashMap bawaan
 		directed = type; // false: undirected, true: directed
 	}
 
@@ -53,7 +50,7 @@ public class Graph<T> {
 		adj.putIfAbsent(a, new MyLinearList<>()); //add node
 		adj.putIfAbsent(b, new MyLinearList<>()); //add node
 		Edge<T> edge1 = new Edge<>(b, w);
-		adj.get(a).pushQ(edge1);//add(edge1); //add edge
+		adj.get(a).pushQ(edge1); //add edge
 		if (!directed) { //undirected
 			Edge<T> edge2 = new Edge<>(a, w);
 			adj.get(b).pushQ(edge2);
@@ -63,7 +60,6 @@ public class Graph<T> {
     //Print graph as hashmap, Time O(V+E), Space O(1)
 	public void printGraph() {
 		for (T key: adj.keySet()) {
-			//System.out.println(key.toString() + " : " + adj.get(key).toString());
             System.out.print(key.toString() + " : ");
 			MyLinearList<Edge<T>> thelist = adj.get(key);
 			Node<Edge<T>> curr = thelist.head;
@@ -75,13 +71,69 @@ public class Graph<T> {
 		}
 	}
 
-	//DFS 
+    // HELPER METHOD: Mengecek apakah node sudah di-visit menggunakan MyLinearList
+    private boolean isVisited(MyLinearList<T> visitedList, T node) {
+        Node<T> curr = visitedList.head;
+        while(curr != null) {
+            if(curr.getData().equals(node)) {
+                return true;
+            }
+            curr = curr.getNext();
+        }
+        return false;
+    }
+
+	// DFS
 	public void DFS(T src) {
-		
+		System.out.print("DFS: ");
+		MyLinearList<T> visited = new MyLinearList<>();
+		DFSUtil(src, visited);
+		System.out.println();
 	}
 
-	//BFS
+	private void DFSUtil(T v, MyLinearList<T> visited) {
+		visited.pushQ(v); // Tandai sebagai visited
+		System.out.print(v + " ");
+		
+		MyLinearList<Edge<T>> list = adj.get(v);
+		if(list != null) {
+			Node<Edge<T>> curr = list.head;
+			while(curr != null) {
+				T neighbor = curr.getData().getNeighbor();
+				if(!isVisited(visited, neighbor)) {
+					DFSUtil(neighbor, visited);
+				}
+				curr = curr.getNext();
+			}
+		}
+	}
+
+	// BFS
 	public void BFS(T src) { 
-		 
+		System.out.print("BFS: ");
+		MyLinearList<T> visited = new MyLinearList<>();
+		MyLinearList<T> queue = new MyLinearList<>(); 
+		
+		visited.pushQ(src);
+		queue.pushQ(src);
+		
+		while(!queue.isEmpty()) {
+			T v = queue.remove();
+			System.out.print(v + " ");
+			
+			MyLinearList<Edge<T>> list = adj.get(v);
+			if(list != null) {
+				Node<Edge<T>> curr = list.head;
+				while(curr != null) {
+					T neighbor = curr.getData().getNeighbor();
+					if(!isVisited(visited, neighbor)) {
+						visited.pushQ(neighbor);
+						queue.pushQ(neighbor);
+					}
+					curr = curr.getNext();
+				}
+			}
+		}
+		System.out.println();
 	}
 }
